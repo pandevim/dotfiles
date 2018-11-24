85eb1be69b34164b41b07253ef64aa900fd91382
Aniruddha Pandey <anirudh.pandev@gmail.com>
2018-11-24 07:59:48 +0530

    Update theme.py

--- .scripts/theme.py
+++ .scripts/theme.py
@@ -2,7 +2,7 @@ import ~/.scripts/wall.py
 
 # https://github.com/BarbUk/dotfiles/blob/master/bin/change_theme
 # change theme according to (gsettings) (crond):
-# day and night (https://github.com/kevinboone/solunar_cmdline)
+# day and night (https://github.com/kevinboone/solunar_cmdline) (https://gist.github.com/Skehmatics/4c683abb2b612434aa304ecc09aad51d)
 # command
 # dark and light
 # sync with wallpaper
