46bac417515f1583bf4ed64d0f1c780d085d173c
Aniruddha Pandey <anirudh.pandev@gmail.com>
2018-12-07 00:20:37 +0530

    Update install.py

--- install.py
+++ install.py
@@ -6,3 +6,18 @@ installation script for dotfiles. This script:
 - creates a virtualenv in the current directory
 - sets up dotfiles in development mode: `python setup.py develop`
 """
+
+# globals
+here = os.path.dirname(os.path.abspath(__file__))
+VIRTUALENV = 'https://raw.github.com/pypa/virtualenv/1.10/virtualenv.py'
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
