1a2f89ff5a4e6b11c5fb01ee4104ce09c72f1098
Aniruddha Pandey <anirudh.pandev@gmail.com>
2018-12-07 00:15:40 +0530

    Update install.py

--- install.py
+++ install.py
@@ -1,2 +1,8 @@
 # type env python3
 #!/usr/bin/env python3
+
+"""
+installation script for dotfiles. This script:
+- creates a virtualenv in the current directory
+- sets up dotfiles in development mode: `python setup.py develop`
+"""
