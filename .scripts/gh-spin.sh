a747ceb982698674bfb6cc9f70b068995ab54d9d
Aniruddha Pandey <anirudh.pandev@gmail.com>
2020-07-19 02:11:23 +0530

    Update gh-spin.sh

--- .scripts/gh-spin.sh
+++ .scripts/gh-spin.sh
@@ -8,11 +8,12 @@ case $description in
 	* )
 		hash=$(echo "$description" | openssl sha256)
 		project_name=${hash#*= }
-		mkdir "$description"
-		cd "$description"
+		mkdir "$project_name"
+		cd "$project_name"
 		echo "This project is generate using [gh-spin](https://github.com/pandevim/dotfiles/blob/master/.scripts/gh-spin.sh) script." >> README.md
 		hub init
 		hub create "$project_name" -d "$description"
+		hub add . && hub commit -m "initial commit" && hub push --set-upstream origin master
 		;;
 
 esac
