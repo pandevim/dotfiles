43c1b2323d88c409770aacc83f4ca64b495631c1
Aniruddha Pandey <anirudh.pandev@gmail.com>
2020-07-19 01:15:17 +0530

    Create gh-spin.sh

---
+++ .scripts/gh-spin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+version=0.0.1
+description=$1
+
+case $description in
+
+	* )
+		hash=$(echo "$description" | openssl sha256)
+		project_name=${hash#*= }
+		mkdir "$description"
+		cd "$description"
+		echo "This project is generate using [gh-spin](pandevim/dotfiles/blob/master/.scripts/gh-spin.sh) script." >> README.md
+		hub init
+		hub create "$project_name" -d "$description"
+		;;
+
+esac
